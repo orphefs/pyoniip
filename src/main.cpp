c98e3d0d11d8977f68676640e1e58cadeb18dafa
Orfeas Kypris <kypriso@gmail.com>
2022-05-14 17:41:27 +0300

    add setup files and lint config

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,49 @@
+#include <iostream>
 #include <pybind11/pybind11.h>
+#include <pybind11/numpy.h>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
 
-PYBIND11_MODULE(cmake_example, m) {
+py::array_t<double> add_arrays(py::array_t<double> input1, py::array_t<double> input2)
+{
+    py::buffer_info buf1 = input1.request();
+    py::buffer_info buf2 = input2.request();
+
+    if (buf1.size != buf2.size)
+    {
+        throw std::runtime_error("Input shapes must match");
+    }
+
+    /*  allocate the buffer */
+    py::array_t<double> result = py::array_t<double>(buf1.size);
+
+    py::buffer_info buf3 = result.request();
+
+    double *ptr1 = (double *)buf1.ptr,
+           *ptr2 = (double *)buf2.ptr,
+           *ptr3 = (double *)buf3.ptr;
+    int X = buf1.shape[0];
+    int Y = buf1.shape[1];
+
+    for (size_t idx = 0; idx < X; idx++)
+    {
+        for (size_t idy = 0; idy < Y; idy++)
+        {
+            ptr3[idx * Y + idy] = ptr1[idx * Y + idy] + ptr2[idx * Y + idy];
+        }
+    }
+
+    // reshape array to match input shape
+    result.resize({X, Y});
+
+    return result;
+}
+
+PYBIND11_MODULE(oniip, m)
+{
     m.doc() = R"pbdoc(
         Pybind11 example plugin
         -----------------------
@@ -20,14 +54,9 @@ PYBIND11_MODULE(cmake_example, m) {
            subtract
     )pbdoc";
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    m.def("add_arrays", &add_arrays, R"pbdoc(
+        Add two arrays
         Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-        Some other explanation about the subtract function.
     )pbdoc");
 
 #ifdef VERSION_INFO
